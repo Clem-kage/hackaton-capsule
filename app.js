d6d82c099e0c551c654f665268ed7c9e0bcfa52a
clément <clemkfr@gmail.com>
2023-11-07 11:24:08 +0100

    scd commit

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const cors = require('cors');
-
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-console.log('ok')
+console.log('okey')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
@@ -16,6 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter);  
 
 module.exports = app;
